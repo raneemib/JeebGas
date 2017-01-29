860a2d3963e9bf2bb4013a183db7b749fa6da08c
Raneem <ramenib@gmail.com>
2017-01-29 05:14:20 +0200

    QoL (Qaulity of Life) Now Clients can get thier adress By a press of a button! just need to finalize the proccess and save everything to sqlite.

--- app/src/main/java/com/raneem/omer/jeebgas/PressUpdateAccount.java
+++ app/src/main/java/com/raneem/omer/jeebgas/PressUpdateAccount.java
@@ -7,10 +7,9 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.AsyncTask;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,30 +18,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import android.location.Location;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.LatLng;
+
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-import static com.raneem.omer.jeebgas.R.id.nametf;
-import static com.raneem.omer.jeebgas.R.id.phonenumbertf;
-import static com.raneem.omer.jeebgas.R.id.addresstf;
 
 
-public class PressUpdateAccount extends AppCompatActivity {
+public class PressUpdateAccount extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    private static final int PERMISSION_ACCESS_COARSE_LOCATION = 1;
+    private GoogleApiClient googleApiClient;
 
-    double latitude;
-    double longitude;
+    double latitude = 0;
+    double longitude = 0;
 
     EditText nametf;
     EditText phonenumbertf;
@@ -69,6 +62,13 @@ public class PressUpdateAccount extends AppCompatActivity {
         nametf.setText(jeebGasClient.getName());
         phonenumbertf.setText(jeebGasClient.getPhone());
 
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { android.Manifest.permission.ACCESS_COARSE_LOCATION },
+                    PERMISSION_ACCESS_COARSE_LOCATION);
+        }
+
+        googleApiClient = new GoogleApiClient.Builder(this, this, this).addApi(LocationServices.API).build();
 
 
     }
@@ -76,16 +76,82 @@ public class PressUpdateAccount extends AppCompatActivity {
 
 // >>>>>>>>>>>>>>>>> START LOCATION TESTING <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_ACCESS_COARSE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // All good!
+                } else {
+                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
+                }
 
-    public void ClickgetLocation(View v) {
-
-        longitude = 0;
-        latitude = 0;
-        GetAddress getAddress = new GetAddress(longitude, latitude);
-        getAddress.execute();
-        Log.d("lng,lat","   " + longitude + "  " + latitude);
+                break;
+        }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(MainActivity.class.getSimpleName(), "Connected to Google Play Services!");
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+            longitude = lastLocation.getLongitude();
+            latitude = lastLocation.getLatitude();
+            Log.d("lng,lat #0","   " + longitude + "  " + latitude);
+
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(MainActivity.class.getSimpleName(), "Can't connect to Google Play Services!");
+    }
+
+// get and display the clients location
+    public void ClickgetLocation(View v) {
+
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+            Log.d("lng,lat #-1", "   " + latitude + "  " + longitude);
+
+            try {
+                longitude = lastLocation.getLongitude(); // get location
+                latitude = lastLocation.getLatitude();
+                Log.d("lng,lat #0", "   " + latitude + "  " + longitude);
+            }catch (Exception e){
+                Log.e("Get Location ", "Error");
+            }
+
+            GetAddress getAddress = new GetAddress(latitude, longitude);// translate and display it
+            getAddress.execute();
+            Log.d("lng,lat #1", "   " + latitude + "  " + longitude);
+        }
+    }
 // >>>>>>>>>>>>>>>>> END LOCATION TESTING <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
